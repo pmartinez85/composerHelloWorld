866f834e8ef321ce47d75843076e2c336438fdd5
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-27 19:11:52 +0200

    last modif

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ require __DIR__ . "/vendor/autoload.php";
 
 require "src/SayHello.php";
 
-$sayhello = new \HelloWorld\SayHello();
-
-echo $sayhello->world();
+//$sayhello = new \HelloWorld\SayHello();
+//
+//echo $sayhello->world();
 
 echo SayHello::world();
 
-$sayhello = new Pmartinez85\SayHello();
-
-echo $sayhello->world();
+//$sayhello = new Pmartinez85\SayHello();
+//
+//echo $sayhello->world();
 
 echo SayHello2::world();
